5d45e6526f428247938c4043bc1b83ca5f97f0f4
Dvir Bartov <dvir.bartov1@gmail.com>
2021-05-26 14:33:01 +0300

    Add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import Button from "./Components/Button/Button";
 
 function App(): JSX.Element {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button
+        text="check"
+        onClick={() => {
+          console.log("hi");
+        }}
+      />
+      <h1>Title</h1>
+      <h2>Subtitle</h2>
+    </div>
+  );
 }
 
 export default App;
